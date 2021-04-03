8fe31fcd87d13c4028a830dfa6dfd8f8fb75e748
anferente20 <anferente10@gmail.com>
2021-04-03 17:11:36 -0500

    Agregando metodos a la VistaGeneral

--- src/presentacion/VistaGeneral.java
+++ src/presentacion/VistaGeneral.java
@@ -1,5 +1,68 @@
 package presentacion;
 
-public class VistaGeneral {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class VistaGeneral extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrincipal frame = new VentanaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+	/**
+	 * Este atributo establece la comunicación con el controlador correspondiente.
+	 */
+	private ControladorVistaGeneral control;
+	
+	/**
+	 * Este atributo establece la comunicación con el modelo.
+	 */
+	private Modelo model;
+	
+	public void capturaEventos() {
+		
+	}
+	
+	
+	public ControladorVistaGeneral getControl() {
+		return control;
+	}
+
+
+	
+
+
+	/**
+	 * Create the frame.
+	 */
+	public VistaGeneral(ControlVistaGeneral control, Modelo model) {
+		this.control = control;
+		this.model = model;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
 
 }
