8b34e9446a35ca45c2afd589d028992c413c21f8
natan <natan@192.168.175.1>
2021-04-06 15:02:20 -0500

    ALU Suma

--- src/logica/ALU.java
+++ src/logica/ALU.java
@@ -9,7 +9,27 @@ public class ALU {
 	 * @return el entero resultante de la suma
 	 */
 	public int sumar(int acumulador, int registroB) {
-		return 0;
+		int resultadosuma = 0;
+		if (acumulador>=0 && registroB>=0) {
+			
+			resultadosuma= acumulador + registroB; 
+		}
+		else {
+			if (acumulador<0 && registroB<0) {
+				resultadosuma= (~acumulador + 1) + (~registroB + 1);	
+			}
+			else {
+				if(acumulador<0 && registroB>=0){
+					resultadosuma= (~acumulador + 1) + registroB;	
+				}
+				else {
+					if(acumulador>=0 && registroB<0) {
+						resultadosuma= acumulador + (~registroB + 1);
+					}
+				}
+			}
+		}	
+		return resultadosuma;
 	}
 	
 	/**
@@ -19,6 +39,12 @@ public class ALU {
 	 * @return el entero resultante de la resta
 	 */
 	public int restar(int acumulador, int registroB) {
-		return 0;
+		
+		int resultadoresta = 0;
+		
+		
+		
+		
+		return resultadoresta;
 	}
 }
